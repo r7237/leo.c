fc22329c194352ee216cd27f7390926484386d95
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-05 13:52:35 +0530

    Update drt.c

--- drt.c
+++ drt.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
+#include<conio.h>
 int main()
 {
     int year;
-
     printf("Enter a year: ");
     scanf("%d",&year);
-
     if(year%4 == 0)
     {
         if( year%100 == 0)
