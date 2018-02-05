9e3660d5e761162f071bcbb94609908b2d0b1995
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-05 14:17:23 +0530

    Update hhhhg.c

--- hhhhg.c
+++ hhhhg.c
@@ -1,22 +1,15 @@
 
 #include <stdio.h>
-
+#include<conio.h>
 int main()
 {
     long long num;
     int count = 0;
-
-    /* Input number from user */
     printf("Enter any number: ");
     scanf("%lld", &num);
-
-    /* Run loop till num is greater than 0 */
     while(num != 0)
     {
-        /* Increment digit count */
         count++;
-
-        /* Remove last digit of 'num' */
         num /= 10;
     }
 
