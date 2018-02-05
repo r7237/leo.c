2e8111296c28f968f81ddee389db130beb147895
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-05 13:13:42 +0530

    Update Rot.c

--- Rot.c
+++ Rot.c
@@ -2,15 +2,11 @@ include <stdio.h>
 int main()
 {
     int number;
-
     printf("Enter an integer: ");
     scanf("%d", &number);
-
-    // True if the number is perfectly divisible by 2
-    if(number % 2 == 0)
+    if(number<=100000)
         printf("%d is even.", number);
     else
         printf("%d is odd.", number);
-
     return 0;
 }
