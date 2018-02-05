578ad4bdb885bfc735c9565e95ee5f512ada03e8
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-05 14:29:11 +0530

    Update ghfff.c

--- ghfff.c
+++ ghfff.c
@@ -2,10 +2,8 @@
 int main()
 {
     int n, i, flag = 0;
-
     printf("Enter a positive integer: ");
     scanf("%d",&n);
-
     for(i=2; i<=n/2; ++i)
     {
         // condition for nonprime number
