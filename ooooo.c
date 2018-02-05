67c3930430d4de3cb85fe0d2c36c7edcd51bff43
r7237 <35914754+r7237@users.noreply.github.com>
2018-02-05 14:21:58 +0530

    Create ooooo.c

---
+++ ooooo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+long power (int, int);
+ 
+int main()
+{
+    int pow, num;
+    long result;
+ 
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Enter it's power: ");
+    scanf("%d", &pow);
+    result = power(num, pow);
+    printf("%d^%d is %ld", num, pow, result);
+    return 0;
+}
